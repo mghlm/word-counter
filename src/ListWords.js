6ef6e52a395783803f2c4f4f8ef3d295985ded83
Magnus Holm <mail@magnusholm.com>
2017-06-12 16:17:53 +0100

    add colors to primenumbers

--- src/ListWords.js
+++ src/ListWords.js
@@ -6,9 +6,9 @@ ListWords.prototype.wrapWordsInHtml = function(array) {
   htmlArray = [];
   for (var i = 0; i < array.length; i++) {
     if (this.isPrimeNumber(array[i][1])) {
-      htmlArray.push('<p> <b>"' + array[i][0]  + '"</b> occures <b>' + array[i][1] + "</b> times " + " --> <b> prime </b>  </p>");
+      htmlArray.push('<p> <b id="word">' + array[i][0]  + '</b> occures <b id="prime-number">' + array[i][1] + '</b> times </p>');
     } else {
-      htmlArray.push('<p> <b>"' + array[i][0]  + '"</b> occures <b>' + array[i][1] + "</b> times " + "</p>");
+      htmlArray.push('<p> <b id="word">' + array[i][0]  + '</b> occures <b id="number">' + array[i][1] + "</b> times " + "</p>");
     }
   }
   return htmlArray;
